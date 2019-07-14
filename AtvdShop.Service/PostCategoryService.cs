bd040139fa0c31763125a66a3aa3844751b551df
atvd <iuhthanhtung4498@gmail.com>
2019-07-14 18:21:36 +0700

    add service

---
+++ AtvdShop.Service/PostCategoryService.cs
@@ -0,0 +1,60 @@
+﻿using AtvdShop.Data.Infrastructure;
+using AtvdShop.Data.Repositories;
+using AtvdShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtvdShop.Service
+{
+    public interface IPostCategoryService
+    {
+        void Add(PostCategory postCategory);
+        void Update(PostCategory postCategory);
+        void Delete(int id);
+        IEnumerable<PostCategory> GetAll();
+        IEnumerable<PostCategory> GetAllByParentID(int parentID);
+        PostCategory GetById(int id);
+    }
+    public class PostCategoryService : IPostCategoryService
+    {
+        IPostCategoryRepository _postCategoryRepository;
+        IUnitOfWork _unitOfWork;
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public void Add(PostCategory postCategory)
+        {
+            _postCategoryRepository.Add(postCategory);
+        }
+
+        public void Delete(int id)
+        {
+            _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentID(int parentID)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentID);
+        }
+
+        public PostCategory GetById(int id)
+        {
+            return _postCategoryRepository.GetSingleByID(id);
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+            _postCategoryRepository.Update(postCategory);
+        }
+    }
+}
